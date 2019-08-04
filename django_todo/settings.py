4887cd21ed0529093a8bc0ab200f191b8dd72958
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-08-04 03:22:55 +0100

    update settings to use eviromental variables

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
+ 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
